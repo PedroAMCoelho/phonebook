ee88af5df76b645f2e726ea7141edaee098370e6
PedroAMCoelho <pedrocoelhoprojetos@gmail.com>
2021-12-29 18:58:56 +0000

    part3 phonebook-backend step4

--- index.js
+++ index.js
@@ -59,6 +59,16 @@ app.get('/api/persons/:id', (req, res) => {
   else res.status(404).end();  
 })
 
+app.delete('/api/persons/:id', (req, res) => {  
+  let id = Number(req.params.id);
+  let exists = contacts.some(x => x.id === id);
+  if (exists) {
+    contacts = contacts.filter(x => x.id !== id);
+    res.status(204).end();
+  }
+  else res.status(404).end();
+})
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
